71f49cca3990ab51307a141f444f3ba7cf4e66fd
Ivo <jack@Ivos-MBP.lan>
2023-02-02 14:17:11 +0200

    Added extra fields for the Create Profile  page

--- src/pages/CreateProfile.js
+++ src/pages/CreateProfile.js
@@ -7,7 +7,7 @@ export function CreateProfile() {
           <div class="md:w-1/3">
             <label
               class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="inline-full-name"
+              for="name"
             >
               Name
             </label>
@@ -18,6 +18,24 @@ export function CreateProfile() {
               id="name"
               type="text"
               placeholder="Name"
+            />
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label
+              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              for="email"
+            >
+              Email
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <input
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-hwite focus:border-purple-500"
+              id="email"
+              type="email"
+              placeholder="Email"
             />
           </div>
         </div>
@@ -47,7 +65,7 @@ export function CreateProfile() {
               class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
               type="button"
             >
-              Sign Up
+              Create Profile
             </button>
           </div>
         </div>
